b43b9c912c262e2a0adcecf4b34ce85261305221
p-etrichor <4my5wn@gmail.com>
2022-02-02 23:59:57 +0900

    Update prob_61.py
    1st try

--- prob_61.py
+++ prob_61.py
@@ -4,7 +4,21 @@ import itertools
 
 print("what is Github")
 
-for i in 10:
+def is_int(x):
+    return x == int(x)
+
+def chk_tri(num):
+    return is_int((2*n+1)**0.5)
+
+def chk_squ(num):
+    return is_Int(num**0.5)
+    
+def chk_penta(num):
+    return is_int()
+
+
+for i in range(10):
   print(i)
+  
   
   
