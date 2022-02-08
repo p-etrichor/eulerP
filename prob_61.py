b67b06433b1a748d434d611c8abfa8b4c5d68829
p-etrichor <4my5wn@gmail.com>
2022-02-08 18:57:04 +0900

    Update prob_61.py

--- prob_61.py
+++ prob_61.py
@@ -1,5 +1,5 @@
 
-print("what is Github")
+import itertools as itt
 
 def chk_num(x, num):
     k = 1
@@ -49,10 +49,54 @@ def chk_num(x, num):
     else:
         return 0
 
-for i in range(10):
-  print(i)
-  
-  
-  
-chk_num(4, 7798)
 
+def chk_true(num):
+    lst = []
+    for i in range(3, 9):
+        if chk_num(i, num) > 0:
+            lst.append(i)
+    return lst
+        
+        
+#f_lst = [''.join(p) for p in itt.combinations_with_replacement('0123456789', 4)]
+t_lst = [''.join(p) for p in itt.combinations_with_replacement('0123456789', 2)]
+
+num = 0
+hoobo_lst = []
+
+
+for i in t_lst:
+    if int(i) > 9:
+        for j in t_lst:
+            num = int(i+j)
+            if len(chk_true(num)) != 0 and int(j) > 9:
+                for k in t_lst:
+                    num = int(j+k)
+                    if len(chk_true(num)) != 0 and int(k) > 9:
+                        for l in t_lst:
+                            num = int(k+l)
+                            if len(chk_true(num)) != 0 and int(l) > 9:
+                                for m in t_lst:
+                                    num = int(l+m)
+                                    if len(chk_true(num)) != 0 and int(m) > 9:
+                                        for n in t_lst:
+                                            num = int(m+n)
+                                            if len(chk_true(num)) != 0 and int(n) > 9:
+                                                num = int(n+i)
+                                                if len(chk_true(num)) != 0:
+                                                    hoobo_lst.append(i+j+k+l+m+n)
+
+            
+for i in hoobo_lst:
+    chk_set = set()
+    chk_set.update(chk_true(int(i[0:4])))
+    chk_set.update(chk_true(int(i[2:6])))
+    chk_set.update(chk_true(int(i[4:8])))
+    chk_set.update(chk_true(int(i[6:10])))
+    chk_set.update(chk_true(int(i[8:12])))
+    chk_set.update(chk_true(int(i[10:12] + i[0:2])))
+    print(chk_set)
+    
+    if len(chk_set) == 6:
+        print (i)
+        
