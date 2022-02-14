1f7d62f4c952f3d7808689cecfa12f9ada25bf8d
p-etrichor <4my5wn@gmail.com>
2022-02-15 01:16:07 +0900

    Update prob_62.py

--- prob_62.py
+++ prob_62.py
@@ -1,2 +1,20 @@
 
+import itertools as itt
+
 cube_lst = []
+
+digit_num = 4
+num = 10
+cube = num ** 3
+
+while len(str(cube)) == digit_num
+    cube_lst.append() 
+    num += 1
+    cube = num ** 3
+    
+for i in cube_lst:
+    chk_lst = []
+    if itt.    
+    
+cube_lst = []
+digit_num += 1
