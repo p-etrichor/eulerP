cd1ca78fa5b89176e8f690ae6d3838b868a6438f
p-etrichor <4my5wn@gmail.com>
2022-02-15 22:55:02 +0900

    Update prob_62.py

--- prob_62.py
+++ prob_62.py
@@ -17,38 +17,43 @@ def chk_num(string):
     
     return rtn
     
-
-cube_set = set()
-
 digit_num = 4
 num = 10
 cube = num ** 3
 
-while len(str(cube)) == digit_num:
-    cube_set.add(str(cube))
-    num += 1
-    cube = num ** 3
+kkk = 0
+
+while kkk != 1:
+    cube_set = set()
     
-# for i in cube_set:
-#     chk_set = set(''.join(p) for p in itt.permutations(i, digit_num))
-#     if len(chk_set & cube_set) == 5:
-#         print (chk_set & cube_set)
+    while len(str(cube)) == digit_num:
+        cube_set.add(str(cube))
+        num += 1
+        cube = num ** 3
+        
+    # for i in cube_set:
+    #     chk_set = set(''.join(p) for p in itt.permutations(i, digit_num))
+    #     if len(chk_set & cube_set) == 5:
+    #         print (chk_set & cube_set)
+        
+        
     
     
-cube_set = set()
-digit_num += 1
+    chk_digit = dict()
+    for i in cube_set:
+        chk = chk_num(i)
+        if chk in chk_digit:
+            chk_digit[chk].append(i)
+        else:
+            chk_digit[chk] = list([i])
+        
+    for i in chk_digit:
+        if len(chk_digit[i]) == 5:
+            print(chk_digit[i])
+            print(min(chk_digit[i]))
+            kkk = 1
+        
+    digit_num += 1
+    
 
 
-
-chk_digit = dict()
-for i in cube_set:
-    chk = chk_num(i)
-    if chk in chk_digit:
-        chk_digit[chk].append(i)
-    else:
-        chk_digit[chk] = list[i]
-    
-for i in chk_digit:
-    if len(chk_digit[i]) == 5:
-        print(chk_digit[i])
-    
